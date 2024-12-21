1a8bb2fd42fa427b33297f696efcb456ab03ebb3
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-21 17:31:36 -0330

    display menu

--- FeetToMeters.py
+++ FeetToMeters.py
@@ -1,10 +1,23 @@
 def main():
+   print("Feet and Meteres Converter")
     
+def conversionmenu():
+   print("Conversions Menu:")
+   print("a. Feet to Meters")
+   print("b. Meters to feet")
+   select_conversion = input("Select a conversion (a/b):").lower()
+   if select_conversion == "a":
+      Convertfeet()
+   elif select_conversion == "b":
+        ConvertMeters()
+    
+   
+
 
 def Convertfeet():
  meters = float("Enter meters")
  feet = meters / 0.3048
- print(meters)
+ print(feet)
 
 def ConvertMeters():
     feet = float("Enter feet")
