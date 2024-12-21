6b7e4de02832e69bbe23453bc923339eacd3a2a1
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-21 16:52:24 -0330

    ask to go again

--- HikeCalculator.py
+++ HikeCalculator.py
@@ -1,7 +1,13 @@
 def main():
+ while True:
     print("Hike calculator")
     miles_walked = float(input("How many miles did you walk?: "))
     convert_miles(miles_walked)
+
+    go_again = input("Another conversion? (y/n)")
+    if go_again != 'y':
+      return False
+      
 
 
 def convert_miles(miles_walked):
