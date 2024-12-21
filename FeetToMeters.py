5d70d0b18100993e93e6a4845f24761e060f7b4f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-21 17:20:33 -0330

    setup functions

---
+++ FeetToMeters.py
@@ -0,0 +1,12 @@
+def main():
+    
+
+def Convertfeet():
+ meters = float("Enter meters")
+ feet = meters / 0.3048
+ print(meters)
+
+def ConvertMeters():
+    feet = float("Enter feet")
+    meters = feet * 0.3048
+    print(meters)
