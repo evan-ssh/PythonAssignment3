6e368d7e177f21bf36f4f7a7ff49eb3c31d1d57d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-23 17:12:38 -0330

    roll again added

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,16 +1,19 @@
 import random
 def main():
- die_one = RollDiceOne()
- die_two = RollDiceTwo()
- total = Total(die_one, die_two)
- print(f"Die 1:", die_one)
- print(f"Die 2:", die_two)
- print(f"Total:", total)
- if die_one == 1 and die_two == 1:
-  print("Snake Eyes")
- elif die_one == 6 and die_two == 6:
-  print("Boxcars")
-  
+ while True:
+    die_one = RollDiceOne()
+    die_two = RollDiceTwo()
+    total = Total(die_one, die_two)
+    print(f"Die 1:", die_one)
+    print(f"Die 2:", die_two)
+    print(f"Total:", total)
+    if die_one == 1 and die_two == 1:
+     print("Snake Eyes")
+    elif die_one == 6 and die_two == 6:
+     print("Boxcars")
+    go_again = input("Roll Again? (y/n)")
+    if go_again != "y":
+     return False
 def RollDiceOne():
  die_one = random.randint(0,6)
  return die_one
