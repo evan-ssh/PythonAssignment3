bccd9bbe8ebb8d2a4e12ab8354c2609c69b5214d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-23 15:30:16 -0330

    Get Total After Tax

--- SalesTaxCalculator.py
+++ SalesTaxCalculator.py
@@ -3,8 +3,10 @@ def main():
   user_items = GetItems()
   total_of_items = (sum(user_items))
   sales_tax = GetSalesTax(total_of_items)
+  total_of_sale = GetTotal(total_of_items, sales_tax)
   print(f"Total: {total_of_items:.2f}")
   print(f"Sales tax: {sales_tax:.2f}")
+  print(f"Total After Tax: {total_of_sale:.2f}")
 
 def GetItems():
     items = []
@@ -20,6 +22,9 @@ def GetSalesTax(total_of_items):
   sales_tax = total_of_items * 0.06
   return sales_tax
 
+def GetTotal(total_of_items, sales_tax):
+  total_of_sale = total_of_items + sales_tax
+  return total_of_sale
 
 
 main()
