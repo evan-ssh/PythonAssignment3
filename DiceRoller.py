98e2262bab8500b23c189f30e8ca24a481d326e3
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-23 17:06:09 -0330

    organized fixed 'and' comparison

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,17 +1,23 @@
 import random
 def main():
- die_one = 6
- #die_one = random.randint(0,6)
+ die_one = RollDiceOne()
+ die_two = RollDiceTwo()
+ total = Total(die_one, die_two)
  print(f"Die 1:", die_one)
- die_two = 6
- #die_two = random.randint(0,6)
  print(f"Die 2:", die_two)
- total = die_one + die_two
  print(f"Total:", total)
- if die_one and die_two == 1:
+ if die_one == 1 and die_two == 1:
   print("Snake Eyes")
- elif die_one and die_two == 6:
+ elif die_one == 6 and die_two == 6:
   print("Boxcars")
   
-
+def RollDiceOne():
+ die_one = random.randint(0,6)
+ return die_one
+def RollDiceTwo():
+ die_two = random.randint(0,6)
+ return die_two
+def Total(die_one,die_two):
+ total = die_one + die_two
+ return total
 main()
