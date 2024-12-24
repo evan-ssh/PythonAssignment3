6a240bf8bd680ad632eb1ab005dec904f97ab025
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-24 15:31:21 -0330

    go again and oraganized code

--- PrimeNumberChecker.py
+++ PrimeNumberChecker.py
@@ -1,12 +1,26 @@
 def main():
+ while True:
     number = int(input("Enter Number: "))
+    IsPrimeNumber(number)
+    PrintFactors(number)
+    go_again = input("Try another number?: ").lower()
+    if go_again != "y":
+        return False
+
+
+def IsPrimeNumber(number):
     for numbers in range(2, number):
         if number % numbers == 0:
-            print("Number is not a prime number")
-            break
+            print(f"{number} is NOT a prime number")
+            break 
         else:
-            print("Number is a prime number")
+            print(f"{number} is a prime number")
+            break
+
+def PrintFactors(number):
+    print("Factors")
     for numbers in range(1, number + 1):
-        if (number % numbers ==0):
-            print(f"factors",numbers)
-main()
+        if number % numbers == 0:
+            print(numbers)
+           
+main() 
